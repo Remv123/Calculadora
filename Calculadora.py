aef6fcbce9b81d88690d673f075b3a1ece78cbe8
Remv123 <rafaelemv0310@gmail.com>
2024-01-14 17:51:36 -0600

    file for the program
    file where the implementation is written

---
+++ Calculadora.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from PyQt5 import QtWidgets,uic
+import sys
+
+
+class Calculadora(QtWidgets.QDialog):
+    def __init__(self):
+        super(Calculadora,self).__init__()
+        uic.loadUi("Calculadora.ui",self)
+        self.Numbers=self.findChild(QtWidgets.QButtonGroup,"Numbers")
+        self.Numbers.buttonClicked.connect(self.SetText)
+        self.Operations=self.findChild(QtWidgets.QButtonGroup,"Operations")
+        self.Operations.buttonClicked.connect(self.SetText)
+        self.Resultado=self.findChild(QtWidgets.QLineEdit,"Resultado")
+        self.Equal=self.findChild(QtWidgets.QPushButton,"Equal")
+        self.Equal.clicked.connect(self.calculateResult)
+        self.show()
+        
+    def ClearText(self):
+        self.Resultado.setText("")
+        
+    def SetText(self,button):
+        if self.GetText()=="ERROR":
+            self.ClearText()
+        Text=self.Resultado.text()
+        Text+=button.text()
+        self.Resultado.setText(Text)
+    
+    def GetText(self):
+        return self.Resultado.text()
+
+    def calculateResult(self):
+        resultado=self.evaluateExpression()
+        self.Resultado.setText(resultado)
+        
+    def evaluateExpression(self):
+        expression=self.GetText()
+        try:
+            result=str(eval(expression))
+        except Exception:
+            result="ERROR"
+        return result
+        
+        
+
+app=QtWidgets.QApplication(sys.argv)
+window=Calculadora()
+app.exec_()
+
+
+
+        
+
